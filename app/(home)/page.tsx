845254babaee87136b5dbe86c245c0623b7848b7
Juan Felici <juan@MBP-de-Juan.fibertel.com.ar>
2024-01-02 14:44:09 -0300

    next js admin dashboard finished

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-	return (
-		<div>
-			<h1>Build Dashboard with role access using Next.js + Supabase </h1>
-		</div>
-	);
+    return (
+        <div className="flex justify-center mt-[25%]">
+            <Link
+                href="/dashboard"
+                className="flex items-center justify-center w-64 h-16 bg-blue-500 rounded-md shadow-md hover:shadow-lg transform hover:scale-105 transition-all"
+            >
+                <h1 className="text-white text-2xl font-bold">Enter .</h1>
+            </Link>
+        </div>
+    );
 }
